953c2cc5cb9a9f93440ac835141f1e814ca83548
Donnie027 <donatiendony27@gmail.com>
2025-02-19 15:00:32 -0600

    Resubir carpeta docs 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/maquetacionWeb-proyectoFinal/'
+  base:'https://donnie027.github.io/maquetacionWeb-proyectoFinal/'
 })
